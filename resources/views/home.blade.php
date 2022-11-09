ec281391464b015ab1b528376695441d4406bb90
mfzeref17 <fadhlanmuhamamd1733@gmail.com>
2022-11-09 13:41:03 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Resume/CV Design</title>
+	<link rel="stylesheet" href="{{asset('stylescv.css')}}">
+  <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="resume">
+   <div class="resume_left">
+     <div class="resume_profile">
+       <img src="{{asset('foto.JPG')}}" alt="profile_pic">
+     </div>
+     <div class="resume_content">
+       <div class="resume_item resume_info">
+         <div class="title">
+           <p class="bold">Muslem</p>
+           <p class="regular">Programmer | Youtuber</p>
+         </div>
+         <ul>
+           <li>
+             <div class="icon">
+               <i class="fas fa-map-signs"></i>
+             </div>
+             <div class="data">
+               Aceh Besar <br /> Indonesia
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-mobile-alt"></i>
+             </div>
+             <div class="data">
+               +6282365205018
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-envelope"></i>
+             </div>
+             <div class="data">
+               muslem19@gmail.com
+             </div>
+           </li>
+           
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">skill's</p>
+         </div>
+         <ul>
+           <li>
+             <div class="skill_name">
+               HTML
+             </div>
+             <div class="skill_progress">
+               <span style="width: 40%;"></span>
+             </div>
+             <div class="skill_per">40%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               CSS
+             </div>
+             <div class="skill_progress">
+               <span style="width: 20%;"></span>
+             </div>
+             <div class="skill_per">20%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               JS
+             </div>
+             <div class="skill_progress">
+               <span style="width: 15%;"></span>
+             </div>
+             <div class="skill_per">15%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               PHP
+             </div>
+             <div class="skill_progress">
+               <span style="width: 20%;"></span>
+             </div>
+             <div class="skill_per">20%</div>
+           </li>
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">Social</p>
+         </div>
+         <ul>
+          <li>
+            <li>      
+            <div class="skill_name">
+                    Indonesia
+                  </div>
+                  <div class="skill_progress">
+                    <span style="width: 80%;"></span>
+                  </div>
+                  <div class="skill_per">80%</div>
+                </li>
+              <li>
+                <div class="skill_name">
+                  English
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 40%;"></span>
+                </div>
+                <div class="skill_per">40%</div>
+              </li>
+              <li>
+                <div class="skill_name">
+                  Aceh
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 70%;"></span>
+                </div>
+                <div class="skill_per">70%</div>
+              </li>
+          </li>
+        </ul>
+       </div>
+     </div>
+  </div>
+  <div class="resume_right">
+    <div class="resume_item resume_about">
+        <div class="title">
+           <p class="bold">About us</p>
+         </div>
+        <p>My Name is Muslem, I'm 20 Years Old, I From Lueng ie, Aceh Besar Island, Indonesia</p>
+        <p>I Graduated from State islamic University of AR-RANIRY From Major Information Technology</p>
+        <p>My Hoby is Playing Games, Climb, and Playing Futsal</p>
+    </div>
+    <div class="resume_item resume_work">
+        <div class="title">
+           <p class="bold">Education</p>
+         </div>
+        <ul>
+            <li>
+              <div class="date">2017 - 2020</div>
+              <div class="info">
+                     <p class="semi-bold">MAN 3 Banda Aceh</p> 
+                  <p>Major: Computer And Network Engineering</p>
+                </div>
+            </li>
+            <li>
+              <div class="date">2020 - 2024</div>
+              <div class="info">
+                     <p class="semi-bold">State islamic University of AR-RANIRY, Banda Aceh</p> 
+                  <p>Major: Information Technology</p>
+                </div>
+            </li>
+            
+          </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_education">
+      <div class="title">
+           <p class="bold">Work Experience</p>
+         </div>
+      <ul>
+            <li>
+                <div class="date">2022 - Present</div> 
+                <div class="info">
+                     <p class="semi-bold">Freelancer Mobile Developer</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2020 - Present</div>
+              <div class="info">
+                <p class="semi-bold">Student</p> 
+                  
+            </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_hobby">
+      <div class="title">
+           <p class="bold">Hobby</p>
+         </div>
+       <ul>
+         <li><i class="fas fa-book"></i></li>
+         <li><i class="fas fa-gamepad"></i></li>
+         <li><i class="fas fa-campground"></i></li>
+         <li><i class="fas fa-futbol"></i></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
